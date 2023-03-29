6b85ad84d3c79954cc271a9c648fc7c67f7c93eb
gcosta2107 <gcosta2003@DESKTOP-RIAUGAK.localdomain>
2023-03-28 21:34:45 -0300

    add temporizador

--- client.py
+++ client.py
@@ -1,37 +1,37 @@
 import socket
-import random
-import time
 
 # Configurações do cliente
-HOST = 'localhost'  # IP do servidor
+HOST = '127.0.0.1'  # IP do servidor
 PORT = 5000  # Porta do servidor
 BUFFER_SIZE = 1024
 
 # Cria um socket TCP/IP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET is the Internet address family for IPv4. SOCK_STREAM is the socket type for TCP
 
 # Conecta-se ao servidor
 client_socket.connect((HOST, PORT))
 print('Conectado ao servidor em', (HOST, PORT))
 
 # Envia mensagens para o servidor
-while True:
-    # Gera uma mensagem aleatória
-    message = input("Digite a mensagem: ")
+with client_socket:
 
-    if message == 'sair':
-        break
+    while True:
+        # Gera uma mensagem aleatória
+        message = input("Digite a mensagem: ")
 
-    # Envia a mensagem
-    client_socket.sendall(message.encode())
+        if message == 'sair':
+            break
 
-    # Espera um tempo aleatório para simular perda de mensagem
-    time.sleep(random.uniform(0, 1))
+        # Envia a mensagem
+        client_socket.sendall(message.encode())
 
-    # Recebe a confirmação do servidor
-    data = client_socket.recv(BUFFER_SIZE)
+        # Espera um tempo aleatório para simular perda de mensagem
+        #time.sleep(random.uniform(0, 1))
 
-    print('Confirmação recebida:', data.decode())
+        # Recebe a confirmação do servidor
+        data = client_socket.recv(BUFFER_SIZE)
+
+        print('Confirmação recebida:', data.decode())
 
 # Fecha a conexão com o servidor
 client_socket.close()
--- server.py
+++ server.py
@@ -1,38 +1,50 @@
+import sys
 import socket
 
 # Configurações do servidor
 HOST = ''  # IP local
 PORT = 5000  # Porta de escuta
-BUFFER_SIZE = 32
+BUFFER_SIZE = 1024
 
 # Cria um socket TCP/IP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Associa o socket a um endereço e porta
-server_socket.bind((HOST, PORT))
+server_socket.bind((HOST, PORT)) #.bind() method is used to associate the socket with a specific network 
 
 # Escuta conexões
-server_socket.listen()
-
+server_socket.listen()  #.listen() enables a server to accept connections
 print('Servidor iniciado em', (HOST, PORT))
 
+# Aceita a conexão do cliente
+client_socket, address = server_socket.accept() #The .accept() method blocks execution and waits for an incoming connection.
+#it returns a new socket object representing the connection(client_socket) and a tuple holding the address of the client(address)
+#A socket function or method that temporarily suspends your application is a blocking call.
+#For example, .accept(), .connect(), .send(), and .recv() block, meaning they don’t return immediately. 
+# Blocking calls have to wait on system calls (I/O) to complete before they can return a value.
+
+client_socket.settimeout(5)
+
+print('Conexão estabelecida com', address)
+
 # Espera por conexões
-while True:
-    # Aceita a conexão do cliente
-    client_socket, address = server_socket.accept()
-    print('Conexão estabelecida com', address)
+with client_socket:
 
     # Recebe mensagens do cliente
     while True:
-        data = client_socket.recv(BUFFER_SIZE)
+        try:
+            data = client_socket.recv(BUFFER_SIZE)
 
-        if not data:
-            break
+            # Confirma recebimento da mensagem
+            client_socket.sendall(b'ACK')
+            
+            print('Mensagem recebida:', data.decode())
 
-        # Confirma recebimento da mensagem
-        client_socket.sendall(b'ACK')
-
-        print('Mensagem recebida:', data.decode())
+            if not data:
+                break
+        except:
+            print("PACOTE PERDIDO")
+            
 
     # Fecha a conexão com o cliente
     client_socket.close()
